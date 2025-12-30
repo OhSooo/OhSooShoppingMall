77b2f832d27187d8e29cf1202ab50a9d6fa601eb
ohyerim025-alt <ohyerim025@gmail.com>
2025-12-30 21:03:15 +0900

    first commit

---
+++ src/test/java/com/ohsooo/platform/ohsooshoppingmall/OhSooShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohsooo.platform.ohsooshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhSooShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
