1b6a77cfe1bb7944acfc018f20619b3954c284a4
soojinsong <162791828+soojinsong@users.noreply.github.com>
2026-01-20 20:26:21 +0900

    Test: 구글 소셜 로그인 테스트

--- src/main/java/com/ohsooo/platform/ohsooshoppingmall/global/config/SecurityConfig.java
+++ src/main/java/com/ohsooo/platform/ohsooshoppingmall/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ohsooo.platform.ohsooshoppingmall.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,11 +12,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form.disable());
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+
+                // 로그인 시작/콜백 등 OAuth2 관련 경로는 열어둠
+                .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+
+
+                .requestMatchers("/me").authenticated()
+
+                .anyRequest().permitAll()
+            )
+            .formLogin(form -> form.disable())
+            .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
