542b0b9c4e277449efc104c7bc50a17cea780377
soojinsong <162791828+soojinsong@users.noreply.github.com>
2026-01-30 04:12:03 +0900

    :sparkles: Feat: globalException에 원인도 받는 생성자 추가

--- src/main/java/com/ohsooo/platform/ohsooshoppingmall/global/exception/BusinessException.java
+++ src/main/java/com/ohsooo/platform/ohsooshoppingmall/global/exception/BusinessException.java
@@ -11,4 +11,11 @@ public class BusinessException extends RuntimeException {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
+
+    // 원인도 받는 생성자 (ex. PG사에서 보내준 에러 메시지)
+    public BusinessException(BaseErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+
 }
